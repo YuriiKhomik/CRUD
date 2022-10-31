364061c3e6a991a27e7b0a0e34ce69cbc3051f4e
YuriiKhomik <105511444+YuriiKhomik@users.noreply.github.com>
2022-10-31 20:53:47 +0000

    Deploying to gh-pages from @ YuriiKhomik/CRUD@d059539636c2e02bd587015692f599a0f4504c14 🚀

--- index.2d369bf7.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,t;o=11,t="".concat("http://localhost:4040","/books/").concat(o),fetch(t,{method:"DELETE"}).then(res=res.json())}();
-//# sourceMappingURL=index.2d369bf7.js.map
--- index.563a3290.js
+++
@@ -1,2 +0,0 @@
-fetch(`http://localhost:4040/books/${11}`,{method:"DELETE"}).then(res=res.json());
-//# sourceMappingURL=index.563a3290.js.map
