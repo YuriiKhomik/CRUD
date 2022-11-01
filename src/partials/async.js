53ab96c1600ee0d4b1dbf67e70b604ba14dffe44
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-11-01 21:58:13 +0200

    ...

--- src/partials/async.js
+++ src/partials/async.js
@@ -101,19 +101,90 @@
 
 // aMakeSmoothie();
 
+// function getFruit(name) {
+//     const fruits = {
+//         strawberry: 'полуничка',
+//         kiwi: 'ківі',
+//         apple: 'ябко',
+//         blackberry: 'ожинка',
+//     };
+//     // // проміс, в який передаємо функцію, всередині якої запускаємо таймаут, всередині якого даємо функцію, яку потрібно запустити
+//     // return new Promise(resolve => setTimeout(() => resolve(fruits[name]), 500));
+
+//     // or:
+
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(fruits[name]);
+//         }, 500);
+//     });
+// }
+
+// // це одна людина ходить за кожним фруктом окремо (тобто функції виконуються по черзі)
+// // і відповідно, такий запит буде виконуватись 2000 мілісекунд
+// // такий варік ок тоді, коли потрібно виконувати кожен наступний проміс по результату іншого
+
+// async function aMakeSmoothie() {
+//     console.time('aMakeSmoothie');
+
+//     const apple = await getFruit('apple');
+//     console.log(apple);
+
+//     const kiwi = await getFruit('kiwi');
+//     console.log(kiwi);
+
+//     const strawberry = await getFruit('strawberry');
+//     console.log(strawberry);
+
+//     const blackberry = await getFruit('blackberry');
+//     console.log(blackberry);
+
+//     console.timeEnd('aMakeSmoothie');
+// }
+
+// aMakeSmoothie();
+
+// // а цей варіант ок тоді, коли потрібно, щоби все разом отримати
+// async function aMakeSmoothie() {
+//     // console.time('aMakeSmoothie');
+
+//     // повертаємо проміси кожного фрукта:
+//     const apple = getFruit('apple');
+//     const kiwi = getFruit('kiwi');
+//     const strawberry = getFruit('strawberry');
+//     const blackberry = getFruit('blackberry');
+
+//     // console.log(apple);
+//     // console.log(kiwi);
+//     // console.log(strawberry);
+//     // console.log(blackberry);
+
+//     // і робимо всі 4 запити на проміси паралельно
+//     // в такому випадку проміси будуть виконані за 500 мілісекунд
+//     // Promise.all чекає виконання всіх промісів і повертає проміс тоді, коли останній проміс ресолвнеться
+//     const fruits = await Promise.all([apple, kiwi, strawberry, blackberry]);
+//     console.log(fruits);
+
+//     // console.timeEnd('aMakeSmoothie');
+// }
+
+// aMakeSmoothie();
+
+// TRY...CATCH
+
 function getFruit(name) {
     const fruits = {
-        strawberry: 'полуниця',
+        strawberry: 'полуничка',
         kiwi: 'ківі',
         apple: 'ябко',
         blackberry: 'ожинка',
     };
-    // // проміс, в який передаємо функцію, всерелині якої запускаємо таймаут, всередині якого даємо функцію, яку потрібно запустити
+    // // проміс, в який передаємо функцію, всередині якої запускаємо таймаут, всередині якого даємо функцію, яку потрібно запустити
     // return new Promise(resolve => setTimeout(() => resolve(fruits[name]), 500));
 
     // or:
 
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(fruits[name]);
         }, 500);
@@ -121,21 +192,19 @@ function getFruit(name) {
 }
 
 async function aMakeSmoothie() {
-    console.time('aMakeSmoothie');
+    try {
+        const apple = getFruit('apple');
+        const kiwi = getFruit('kiwi');
+        const strawberry = getFruit('strawberry');
+        const blackberry = getFruit('blackberry');
 
-    const apple = await getFruit('apple');
-    console.log(apple);
-
-    const kiwi = await getFruit('kiwi');
-    console.log(kiwi);
-
-    const strawberry = await getFruit('strawberry');
-    console.log(strawberry);
-
-    const blackberry = await getFruit('blackberry');
-    console.log(blackberry);
-
-    console.timeEnd('aMakeSmoothie');
+        const fruits = await Promise.all([apple, kiwi, strawberry, blackberry]);
+        console.log(fruits);
+        // асинхронна функція повертає проміс і те, що ми повернемо у зовнішній код - буде результат цього промісу
+        return fruits;
+    } catch (error) {
+        console.log(error);
+    }
 }
-
-aMakeSmoothie();
+// асинхронна функція повертає проміс і те, що ми повернемо у зовнішній код - буде результат цього промісу
+aMakeSmoothie().then(fruits => console.log(fruits));
