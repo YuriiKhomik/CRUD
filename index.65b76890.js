20302dcaeea0a9898eaf8a6ddb4afd34ddeac65d
YuriiKhomik <105511444+YuriiKhomik@users.noreply.github.com>
2022-11-02 10:10:11 +0000

    Deploying to gh-pages from @ YuriiKhomik/CRUD@6ae0f51d9f350c62dc8a1a2cc12a6a26694c24ce 🚀

---
+++ index.65b76890.js
@@ -0,0 +1,2 @@
+!async function(t,o){const n={method:"PATCH",headers:{"Content-Type":"application/json"},body:JSON.stringify(t)},a=await fetch(`http://localhost:4040/books/${o}`,n);await a.json()}({title:"My Superb Amazing Book"},12);
+//# sourceMappingURL=index.65b76890.js.map
